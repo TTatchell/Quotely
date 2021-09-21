02a7f4a752002a20c5c8081601e01ec99624d62a
TTatchell <tim.tatchell24@gmail.com>
2021-09-21 21:24:17 +1000

    Reset password logic

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,6 +4,14 @@
 class PasswordResetsController < ApplicationController
   def new; end
 
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user.present?
+      PasswordMailer.with(user: @user).reset.deliver_later
+    end
+    redirect_to root_path, notice: "The password reset email has been sent"
+  end
+
   def edit
     @user = User.find_signed!(params[:token], purpose: 'password_reset')
   rescue ActiveSupport::MessageVerifier::InvalidSignature
