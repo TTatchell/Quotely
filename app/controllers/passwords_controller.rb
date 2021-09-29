c70b0fcfe27a98517b9c8dd160491336a5484383
TTatchell <tim.tatchell24@gmail.com>
2021-09-29 23:24:27 +1000

    Fixed Edit Password + CSS

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Handles password changes
-class PasswordController < ApplicationController
+class PasswordsController < ApplicationController
   before_action :require_user_logged_in!
   def edit; end
 
